e25b60952567f24794358e930132adeb39afc0cd
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2023-07-24 00:27:13 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
+const API_URL = 'http://localhost:5000/api/data';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const jsonData = await response.json();
+      setData(jsonData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="my-4">Responsive Table</h1>
+      <div className="table-responsive">
+        <table className="table table-striped table-bordered">
+          <thead className="thead-dark">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+              {}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                {}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+    
